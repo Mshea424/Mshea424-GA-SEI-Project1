a308eb5a6e20802ed4f9b86cc8a8757cfe52a9b0
Mike Shea <mshea424@yahoo.com>
2020-04-09 14:47:18 -0400

    introduced alert for broken key edge case & programmed all Keyboard divs

--- script.js
+++ script.js
@@ -1,31 +1,73 @@
-
-
 let l = document.querySelector(`.l`)
-const kq = document.querySelector(`#kq`)
-const kw = document.querySelector(`#kw`)
+
+// Comparison function
 function keycompare(elmt) {
-    console.log()
     if (l.innerText === elmt.innerText) {
     console.log(`Win!`)
-} else {
+    } else {
     console.log(`Lose!`)
+    }
 }
-// console.log(`compare`)
-}
-kq.addEventListener(`click`, (evt) => {keycompare(kq); console.log(evt); evt.target.classList.remove('k'); evt.target.classList.add('bk')})
-kw.addEventListener(`click`, () => keycompare(kw))
+// KBLine 1 variables
+const kq = document.querySelector(`#kq`)
+const kw = document.querySelector(`#kw`)
+const ke = document.querySelector(`#ke`)
+const kr = document.querySelector(`#kr`)
+const kt = document.querySelector(`#kt`)
+const ky = document.querySelector(`#ky`)
+const ku = document.querySelector(`#ku`)
+const ki = document.querySelector(`#ki`)
+const ko = document.querySelector(`#ko`)
+const kp = document.querySelector(`#kp`)
 
-// kq.addEventListener(`click`, () => {
-//     if (l.innerText === kq.innerText) {
-//         console.log(`Win!`)
-//     } else {
-//         console.log(`Lose!`)
-//     }
-// })
-// kw.addEventListener(`click`, () => {
-//     if (l.innerText === kw.innerText) {
-//         console.log(`Win!`)
-//     } else {
-//         console.log(`Lose!`)
-//     }
-// })
+//KBline 2 variables
+const ka = document.querySelector(`#ka`)
+const ks = document.querySelector(`#ks`)
+const kd = document.querySelector(`#kd`)
+const kf = document.querySelector(`#kf`)
+const kg = document.querySelector(`#kg`)
+const kh = document.querySelector(`#kh`)
+const kj = document.querySelector(`#kj`)
+const kk = document.querySelector(`#kk`)
+const kl = document.querySelector(`#kl`)
+
+//KBline 3 variables
+const kz = document.querySelector(`#kz`)
+const kx = document.querySelector(`#kx`)
+const kc = document.querySelector(`#kc`)
+const kv = document.querySelector(`#kv`)
+const kb = document.querySelector(`#kb`)
+const kn = document.querySelector(`#kn`)
+const km = document.querySelector(`#km`)
+
+//KBline 1 Listeners
+kq.addEventListener(`click`, (evt) => {if(evt.target.classList.value === 'k') {keycompare(kq); evt.target.classList.remove('k'); evt.target.classList.add('bk')} else {alert(`"Flutterby" Key is Broken!`)}})
+kw.addEventListener(`click`, (evt) => {if(evt.target.classList.value === 'k') {keycompare(kw); evt.target.classList.remove('k'); evt.target.classList.add('bk')} else {alert(`"Flutterby" Key is Broken!`)}})
+ke.addEventListener(`click`, (evt) => {if(evt.target.classList.value === 'k') {keycompare(ke); evt.target.classList.remove('k'); evt.target.classList.add('bk')} else {alert(`"Flutterby" Key is Broken!`)}})
+kr.addEventListener(`click`, (evt) => {if(evt.target.classList.value === 'k') {keycompare(kr); evt.target.classList.remove('k'); evt.target.classList.add('bk')} else {alert(`"Flutterby" Key is Broken!`)}})
+kt.addEventListener(`click`, (evt) => {if(evt.target.classList.value === 'k') {keycompare(kt); evt.target.classList.remove('k'); evt.target.classList.add('bk')} else {alert(`"Flutterby" Key is Broken!`)}})
+ky.addEventListener(`click`, (evt) => {if(evt.target.classList.value === 'k') {keycompare(ky); evt.target.classList.remove('k'); evt.target.classList.add('bk')} else {alert(`"Flutterby" Key is Broken!`)}})
+ku.addEventListener(`click`, (evt) => {if(evt.target.classList.value === 'k') {keycompare(ku); evt.target.classList.remove('k'); evt.target.classList.add('bk')} else {alert(`"Flutterby" Key is Broken!`)}})
+ki.addEventListener(`click`, (evt) => {if(evt.target.classList.value === 'k') {keycompare(ki); evt.target.classList.remove('k'); evt.target.classList.add('bk')} else {alert(`"Flutterby" Key is Broken!`)}})
+ko.addEventListener(`click`, (evt) => {if(evt.target.classList.value === 'k') {keycompare(ko); evt.target.classList.remove('k'); evt.target.classList.add('bk')} else {alert(`"Flutterby" Key is Broken!`)}})
+kp.addEventListener(`click`, (evt) => {if(evt.target.classList.value === 'k') {keycompare(kp); evt.target.classList.remove('k'); evt.target.classList.add('bk')} else {alert(`"Flutterby" Key is Broken!`)}})
+
+//KBline 2 Listeners
+ka.addEventListener(`click`, (evt) => {if(evt.target.classList.value === 'k') {keycompare(ka); evt.target.classList.remove('k'); evt.target.classList.add('bk')} else {alert(`"Flutterby" Key is Broken!`)}})
+ks.addEventListener(`click`, (evt) => {if(evt.target.classList.value === 'k') {keycompare(ks); evt.target.classList.remove('k'); evt.target.classList.add('bk')} else {alert(`"Flutterby" Key is Broken!`)}})
+kd.addEventListener(`click`, (evt) => {if(evt.target.classList.value === 'k') {keycompare(kd); evt.target.classList.remove('k'); evt.target.classList.add('bk')} else {alert(`"Flutterby" Key is Broken!`)}})
+kf.addEventListener(`click`, (evt) => {if(evt.target.classList.value === 'k') {keycompare(kf); evt.target.classList.remove('k'); evt.target.classList.add('bk')} else {alert(`"Flutterby" Key is Broken!`)}})
+kg.addEventListener(`click`, (evt) => {if(evt.target.classList.value === 'k') {keycompare(kg); evt.target.classList.remove('k'); evt.target.classList.add('bk')} else {alert(`"Flutterby" Key is Broken!`)}})
+kh.addEventListener(`click`, (evt) => {if(evt.target.classList.value === 'k') {keycompare(kh); evt.target.classList.remove('k'); evt.target.classList.add('bk')} else {alert(`"Flutterby" Key is Broken!`)}})
+kj.addEventListener(`click`, (evt) => {if(evt.target.classList.value === 'k') {keycompare(kj); evt.target.classList.remove('k'); evt.target.classList.add('bk')} else {alert(`"Flutterby" Key is Broken!`)}})
+kk.addEventListener(`click`, (evt) => {if(evt.target.classList.value === 'k') {keycompare(kk); evt.target.classList.remove('k'); evt.target.classList.add('bk')} else {alert(`"Flutterby" Key is Broken!`)}})
+kl.addEventListener(`click`, (evt) => {if(evt.target.classList.value === 'k') {keycompare(kl); evt.target.classList.remove('k'); evt.target.classList.add('bk')} else {alert(`"Flutterby" Key is Broken!`)}})
+
+//KBline 3 Listeners
+kz.addEventListener(`click`, (evt) => {if(evt.target.classList.value === 'k') {keycompare(kz); evt.target.classList.remove('k'); evt.target.classList.add('bk')} else {alert(`"Flutterby" Key is Broken!`)}})
+kx.addEventListener(`click`, (evt) => {if(evt.target.classList.value === 'k') {keycompare(kx); evt.target.classList.remove('k'); evt.target.classList.add('bk')} else {alert(`"Flutterby" Key is Broken!`)}})
+kc.addEventListener(`click`, (evt) => {if(evt.target.classList.value === 'k') {keycompare(kc); evt.target.classList.remove('k'); evt.target.classList.add('bk')} else {alert(`"Flutterby" Key is Broken!`)}})
+kv.addEventListener(`click`, (evt) => {if(evt.target.classList.value === 'k') {keycompare(kv); evt.target.classList.remove('k'); evt.target.classList.add('bk')} else {alert(`"Flutterby" Key is Broken!`)}})
+kb.addEventListener(`click`, (evt) => {if(evt.target.classList.value === 'k') {keycompare(kb); evt.target.classList.remove('k'); evt.target.classList.add('bk')} else {alert(`"Flutterby" Key is Broken!`)}})
+kn.addEventListener(`click`, (evt) => {if(evt.target.classList.value === 'k') {keycompare(kn); evt.target.classList.remove('k'); evt.target.classList.add('bk')} else {alert(`"Flutterby" Key is Broken!`)}})
+km.addEventListener(`click`, (evt) => {if(evt.target.classList.value === 'k') {keycompare(km); evt.target.classList.remove('k'); evt.target.classList.add('bk')} else {alert(`"Flutterby" Key is Broken!`)}})
