7551ff86e1e5653500bc3fae2877bc2ae5b9bdb2
Mike Shea <mshea424@yahoo.com>
2020-04-09 10:57:28 -0400

    added win-lose logice for single letter & two inputs

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log('javascript works!')
+
+
+let l = document.querySelector(`.l`)
+const kq = document.querySelector(`#kq`)
+const kw = document.querySelector(`#kw`)
+console.log(l.innerText)
+console.log(kq.innerText)
+console.log(kw.innerText)
+kq.addEventListener(`click`, () => {
+    if (l.innerText === kq.innerText) {
+    console.log(`Win!`)
+} else {
+    console.log(`Lose!`)
+}
+})
+kw.addEventListener(`click`, () => {
+    if (l.innerText === kw.innerText) {
+    console.log(`Win!`)
+} else {
+    console.log(`Lose!`)
+}
+})
