1099aac63d569b90d3995aeb751683403bc5cac3
Mike Shea <mshea424@yahoo.com>
2020-04-09 12:15:55 -0400

    formatted keypress listener to be scalable

--- script.js
+++ script.js
@@ -3,20 +3,29 @@
 let l = document.querySelector(`.l`)
 const kq = document.querySelector(`#kq`)
 const kw = document.querySelector(`#kw`)
-console.log(l.innerText)
-console.log(kq.innerText)
-console.log(kw.innerText)
-kq.addEventListener(`click`, () => {
-    if (l.innerText === kq.innerText) {
+function keycompare(elmt) {
+    console.log()
+    if (l.innerText === elmt.innerText) {
     console.log(`Win!`)
 } else {
     console.log(`Lose!`)
 }
-})
-kw.addEventListener(`click`, () => {
-    if (l.innerText === kw.innerText) {
-    console.log(`Win!`)
-} else {
-    console.log(`Lose!`)
+// console.log(`compare`)
 }
-})
+kq.addEventListener(`click`, (evt) => {keycompare(kq); console.log(evt); evt.target.classList.remove('k'); evt.target.classList.add('bk')})
+kw.addEventListener(`click`, () => keycompare(kw))
+
+// kq.addEventListener(`click`, () => {
+//     if (l.innerText === kq.innerText) {
+//         console.log(`Win!`)
+//     } else {
+//         console.log(`Lose!`)
+//     }
+// })
+// kw.addEventListener(`click`, () => {
+//     if (l.innerText === kw.innerText) {
+//         console.log(`Win!`)
+//     } else {
+//         console.log(`Lose!`)
+//     }
+// })
